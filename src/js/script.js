159aaff8772466eebd7dd5ee79ee76cd5d379d4d
mathieu <mathecool22@gmail.com>
2022-04-05 16:50:48 -0400

    changement apporter au footer + home mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,61 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
+	//Un commentaire
+	console.log("Ça fonctionne!!!");
 
-    //Un commentaire
-    console.log("Ça fonctionne!!!");
+	const uneFonction = () => {
+		console.log("coucou");
+	};
 
-    const uneFonction = ()=>{
-        console.log('coucou');
-    }
-
-    uneFonction();
-
-    let prev = document.getElementById('prev');
-    let next = document.getElementById('next');
-    let slides = document.getElementById('slides');
-    let pos = 0;
-    const tailleImage = 200;
-    console.log($('.slides').css('margin-left'));
-    console.log(pos);
-    prev.addEventListener('click', ()=>{
-            console.log('prev');
-           
-            if($('.slides').css('margin-left')==="0px"||pos<=0){
-                pos=6;
-                $('.slides').finish().animate({
-                    'marginLeft':"-1200px"
-                },500)
-                console.log('0');
-            }else{
-                $('.slides')
-                .finish()
-                .animate({
-                    'marginLeft': "+="+tailleImage+"px"
-                }, 300);
-                pos--;
-                console.log(pos);
-                console.log($('.slides').css('margin-left'));
-            }
-    });
-
-    next.addEventListener('click', ()=>{
-        console.log('next');
-        if($('.slides').css('margin-left')==="-1200px"||pos>=6){
-            pos=0;
-                $('.slides').finish().animate({
-                    'marginLeft':"0px"
-                }, 500)
-            console.log('0');
-        }else{
-            $('.slides')
-            .finish()
-            .animate({
-                'marginLeft': "-="+tailleImage+"px"
-            }, 300);
-            pos++;
-            console.log(pos);
-            console.log($('.slides').css('margin-left'));
-        }
-        //console.log($('.slides').css('margin-left'));
-});
-
+	uneFonction();
 });
