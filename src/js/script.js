9f290202f8c9a05cb693a64a71486d02c50e1ce5
mathieu <mathecool22@gmail.com>
2022-04-07 15:44:52 -0400

    reparer le bug d'affichage dans la navbar sur le mobile

--- src/js/script.js
+++ src/js/script.js
@@ -13,13 +13,42 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	const toggleLeMenu = () => {
 		console.log("cesame ouvre toi!");
 
-		menuHam.classList.toggle("toggle-off");
-		menuHam.classList.toggle("toggle-on");
+		if (!menuHam.classList.contains("toggle-off")) {
+			menuHam.classList.add("toggle-off");
+		} else if (menuHam.classList.contains("toggle-off")) {
+			menuHam.classList.remove("toggle-off");
+		}
+
+		if (!menuHam.classList.contains("toggle-on")) {
+			menuHam.classList.add("toggle-on");
+		} else if (menuHam.classList.contains("toggle-on")) {
+			menuHam.classList.remove("toggle-on");
+		}
 
 		console.log(logoNav.src);
-		navM.classList.toggle("blanc-nav");
-		hamPain1.classList.toggle("pain2");
-		hamPain2.classList.toggle("pain2");
+		if (!navM.classList.contains("blanc-nav")) {
+			navM.classList.add("blanc-nav");
+		} else if (
+			navM.classList.contains("blanc-nav") &&
+			window.scrollY <= 10
+		) {
+			navM.classList.remove("blanc-nav");
+		}
+
+		if (
+			!hamPain1.classList.contains("pain2") &&
+			!hamPain2.classList.contains("pain2")
+		) {
+			hamPain1.classList.add("pain2");
+			hamPain2.classList.add("pain2");
+		} else if (
+			hamPain1.classList.contains("pain2") &&
+			hamPain2.classList.contains("pain2") &&
+			window.scrollY <= 10
+		) {
+			hamPain1.classList.remove("pain2");
+			hamPain2.classList.remove("pain2");
+		}
 
 		//si l'image est blanche...
 		if (logoImg === "logo-blanc.svg") {
@@ -27,7 +56,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 			logoImg = "logo-noir.svg";
 			logoNav.src = "../images/" + logoImg;
 			//sinon si l'image est noir...
-		} else if (logoImg === "logo-noir.svg") {
+		} else if (logoImg === "logo-noir.svg" && window.scrollY <= 10) {
 			logoImg = "logo-blanc.svg";
 			logoNav.src = "../images/" + logoImg;
 		}
@@ -51,7 +80,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 			// navM.classList.remove("blanc-nav");
 			// hamPain1.classList.remove("pain2");
 			// hamPain2.classList.remove("pain2");
-			if (menuHam.classList.contains("toggle-off")) {
+			if (
+				menuHam.classList.contains("toggle-off") &&
+				window.screenY <= 10
+			) {
 				logoImg = "logo-blanc.svg";
 				logoNav.src = "../images/" + logoImg;
 				navM.classList.remove("blanc-nav");
