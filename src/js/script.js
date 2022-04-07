6448fde5f0b9fbcde248359266b147e5b6f721dd
mathieu <mathecool22@gmail.com>
2022-04-07 11:29:07 -0400

    avancement dans le contenu du menu ham

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,40 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 	//Un commentaire
 	console.log("Ça fonctionne!!!");
+	const menuHam = document.querySelector("#ham");
+	const btnHam = document.querySelector(".cont-ham");
+	const navM = document.querySelector(".nav-m");
+	const logoNav = document.querySelector("#logo-nav-m");
+	const hamPain1 = document.querySelector("#pain1");
+	const hamPain2 = document.querySelector("#pain2");
 
-	const uneFonction = () => {
-		console.log("coucou");
+	let logoImg = logoNav.src.split("/").pop();
+
+	const toggleLeMenu = () => {
+		console.log("cesame ouvre toi!");
+
+		menuHam.classList.toggle("toggle-off");
+		menuHam.classList.toggle("toggle-on");
+
+		console.log(logoNav.src);
+		navM.classList.toggle("blanc-nav");
+		hamPain1.classList.toggle("pain2");
+		hamPain2.classList.toggle("pain2");
+
+		//si l'image est blanche...
+		if (logoImg === "logo-blanc.svg") {
+			console.log("change l'image!");
+			logoImg = "logo-noir.svg";
+			logoNav.src = "../images/" + logoImg;
+			//sinon si l'image est noir...
+		} else if (logoImg === "logo-noir.svg") {
+			logoImg = "logo-blanc.svg";
+			logoNav.src = "../images/" + logoImg;
+		}
 	};
 
-	uneFonction();
+	btnHam.addEventListener("click", toggleLeMenu);
+	const uneFonction = () => {
+		// console.log("coucou");
+	};
 });
