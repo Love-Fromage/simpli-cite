f84fe21fade926248a54d04ae0a72c4334cdaad6
mathieu <mathecool22@gmail.com>
2022-04-11 16:01:34 -0400

    ajustement du html et script pour differencier home des autres

--- src/js/script.js
+++ src/js/script.js
@@ -25,73 +25,76 @@ document.addEventListener("DOMContentLoaded", function (event) {
 			menuHam.classList.remove("toggle-on");
 		}
 
-		console.log(logoNav.src);
-		if (!navM.classList.contains("blanc-nav")) {
-			navM.classList.add("blanc-nav");
-		} else if (
-			navM.classList.contains("blanc-nav") &&
-			window.scrollY <= 10
-		) {
-			navM.classList.remove("blanc-nav");
-		}
-
-		if (
-			!hamPain1.classList.contains("pain2") &&
-			!hamPain2.classList.contains("pain2")
-		) {
-			hamPain1.classList.add("pain2");
-			hamPain2.classList.add("pain2");
-		} else if (
-			hamPain1.classList.contains("pain2") &&
-			hamPain2.classList.contains("pain2") &&
-			window.scrollY <= 10
-		) {
-			hamPain1.classList.remove("pain2");
-			hamPain2.classList.remove("pain2");
-		}
-
-		//si l'image est blanche...
-		if (logoImg === "logo-blanc.svg") {
-			console.log("change l'image!");
-			logoImg = "logo-noir.svg";
-			logoNav.src = "../images/" + logoImg;
-			//sinon si l'image est noir...
-		} else if (logoImg === "logo-noir.svg" && window.scrollY <= 10) {
-			logoImg = "logo-blanc.svg";
-			logoNav.src = "../images/" + logoImg;
-		}
-	};
-	console.log(window.pageYOffset);
-
-	btnHam.addEventListener("click", toggleLeMenu);
-	window.addEventListener("scroll", () => {
-		// console.log(window.pageYOffset);
-		if (window.pageYOffset >= 10) {
-			// console.log("plus bas de 10");
-			navM.classList.add("blanc-nav");
-			hamPain1.classList.add("pain2");
-			hamPain2.classList.add("pain2");
-
-			logoImg = "logo-noir.svg";
-			logoNav.src = "../images/" + logoImg;
-		} else {
-			// logoImg = "logo-blanc.svg";
-			// logoNav.src = "../images/" + logoImg;
-			// navM.classList.remove("blanc-nav");
-			// hamPain1.classList.remove("pain2");
-			// hamPain2.classList.remove("pain2");
-			if (
-				menuHam.classList.contains("toggle-off") &&
-				window.screenY <= 10
+		//fait juste ca si tes sur la page d'accueil please
+		if (document.querySelector(".home-cont-about") != null) {
+			console.log(logoNav.src);
+			if (!navM.classList.contains("blanc-nav")) {
+				navM.classList.add("blanc-nav");
+			} else if (
+				navM.classList.contains("blanc-nav") &&
+				window.scrollY <= 10
 			) {
+				navM.classList.remove("blanc-nav");
+			}
+			if (logoImg === "logo-blanc.svg") {
+				console.log("change l'image!");
+				logoImg = "logo-noir.svg";
+				logoNav.src = "../images/" + logoImg;
+				//sinon si l'image est noir...
+			} else if (logoImg === "logo-noir.svg" && window.scrollY <= 10) {
 				logoImg = "logo-blanc.svg";
 				logoNav.src = "../images/" + logoImg;
-				navM.classList.remove("blanc-nav");
+			}
+			window.addEventListener("scroll", () => {
+				// console.log(window.pageYOffset);
+				if (window.pageYOffset >= 10) {
+					// console.log("plus bas de 10");
+					navM.classList.add("blanc-nav");
+					hamPain1.classList.add("pain2");
+					hamPain2.classList.add("pain2");
+
+					logoImg = "logo-noir.svg";
+					logoNav.src = "../images/" + logoImg;
+				} else {
+					// logoImg = "logo-blanc.svg";
+					// logoNav.src = "../images/" + logoImg;
+					// navM.classList.remove("blanc-nav");
+					// hamPain1.classList.remove("pain2");
+					// hamPain2.classList.remove("pain2");
+					if (
+						menuHam.classList.contains("toggle-off") &&
+						window.screenY <= 10
+					) {
+						logoImg = "logo-blanc.svg";
+						logoNav.src = "../images/" + logoImg;
+						navM.classList.remove("blanc-nav");
+						hamPain1.classList.remove("pain2");
+						hamPain2.classList.remove("pain2");
+					}
+				}
+			});
+			if (
+				!hamPain1.classList.contains("pain2") &&
+				!hamPain2.classList.contains("pain2")
+			) {
+				hamPain1.classList.add("pain2");
+				hamPain2.classList.add("pain2");
+			} else if (
+				hamPain1.classList.contains("pain2") &&
+				hamPain2.classList.contains("pain2") &&
+				window.scrollY <= 10
+			) {
 				hamPain1.classList.remove("pain2");
 				hamPain2.classList.remove("pain2");
 			}
 		}
-	});
+
+		//si l'image est blanche...
+	};
+	console.log(window.pageYOffset);
+
+	btnHam.addEventListener("click", toggleLeMenu);
+
 	const uneFonction = () => {
 		// console.log("coucou");
 	};
