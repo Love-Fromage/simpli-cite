262b8461f27b91ac53545976ef75407e05bd7531
mathieu <mathecool22@gmail.com>
2022-04-11 16:42:23 -0400

    correction d'un bug mais pas fini. bug a l'init de page

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,69 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	const hamPain2 = document.querySelector("#pain2");
 
 	let logoImg = logoNav.src.split("/").pop();
+	if (document.querySelector(".home-cont-about") !== null) {
+		window.addEventListener("scroll", () => {
+			console.log(window.pageYOffset);
+			if (window.pageYOffset !== 0) {
+				// console.log("plus bas de 10");
+				navM.classList.add("blanc-nav");
+				hamPain1.classList.add("pain2");
+				hamPain2.classList.add("pain2");
+
+				logoImg = "logo-noir.svg";
+				logoNav.src = "../images/" + logoImg;
+			} else {
+				// logoImg = "logo-blanc.svg";
+				// logoNav.src = "../images/" + logoImg;
+				// navM.classList.remove("blanc-nav");
+				// hamPain1.classList.remove("pain2");
+				// hamPain2.classList.remove("pain2");
+				if (
+					menuHam.classList.contains("toggle-off") &&
+					window.screenY <= 10
+				) {
+					logoImg = "logo-blanc.svg";
+					logoNav.src = "../images/" + logoImg;
+					navM.classList.remove("blanc-nav");
+					hamPain1.classList.remove("pain2");
+					hamPain2.classList.remove("pain2");
+				}
+			}
+		});
+		console.log(logoNav.src);
+		if (!navM.classList.contains("blanc-nav")) {
+			navM.classList.add("blanc-nav");
+		} else if (
+			navM.classList.contains("blanc-nav") &&
+			window.scrollY <= 10
+		) {
+			navM.classList.remove("blanc-nav");
+		}
+		if (logoImg === "logo-blanc.svg") {
+			console.log("change l'image!");
+			logoImg = "logo-noir.svg";
+			logoNav.src = "../images/" + logoImg;
+			//sinon si l'image est noir...
+		} else if (logoImg === "logo-noir.svg" && window.scrollY <= 10) {
+			logoImg = "logo-blanc.svg";
+			logoNav.src = "../images/" + logoImg;
+		}
+
+		if (
+			!hamPain1.classList.contains("pain2") &&
+			!hamPain2.classList.contains("pain2")
+		) {
+			hamPain1.classList.add("pain2");
+			hamPain2.classList.add("pain2");
+		} else if (
+			hamPain1.classList.contains("pain2") &&
+			hamPain2.classList.contains("pain2") &&
+			window.scrollY <= 10
+		) {
+			hamPain1.classList.remove("pain2");
+			hamPain2.classList.remove("pain2");
+		}
+	}
 
 	const toggleLeMenu = () => {
 		console.log("cesame ouvre toi!");
@@ -26,68 +89,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 		}
 
 		//fait juste ca si tes sur la page d'accueil please
-		if (document.querySelector(".home-cont-about") != null) {
-			console.log(logoNav.src);
-			if (!navM.classList.contains("blanc-nav")) {
-				navM.classList.add("blanc-nav");
-			} else if (
-				navM.classList.contains("blanc-nav") &&
-				window.scrollY <= 10
-			) {
-				navM.classList.remove("blanc-nav");
-			}
-			if (logoImg === "logo-blanc.svg") {
-				console.log("change l'image!");
-				logoImg = "logo-noir.svg";
-				logoNav.src = "../images/" + logoImg;
-				//sinon si l'image est noir...
-			} else if (logoImg === "logo-noir.svg" && window.scrollY <= 10) {
-				logoImg = "logo-blanc.svg";
-				logoNav.src = "../images/" + logoImg;
-			}
-			window.addEventListener("scroll", () => {
-				// console.log(window.pageYOffset);
-				if (window.pageYOffset >= 10) {
-					// console.log("plus bas de 10");
-					navM.classList.add("blanc-nav");
-					hamPain1.classList.add("pain2");
-					hamPain2.classList.add("pain2");
-
-					logoImg = "logo-noir.svg";
-					logoNav.src = "../images/" + logoImg;
-				} else {
-					// logoImg = "logo-blanc.svg";
-					// logoNav.src = "../images/" + logoImg;
-					// navM.classList.remove("blanc-nav");
-					// hamPain1.classList.remove("pain2");
-					// hamPain2.classList.remove("pain2");
-					if (
-						menuHam.classList.contains("toggle-off") &&
-						window.screenY <= 10
-					) {
-						logoImg = "logo-blanc.svg";
-						logoNav.src = "../images/" + logoImg;
-						navM.classList.remove("blanc-nav");
-						hamPain1.classList.remove("pain2");
-						hamPain2.classList.remove("pain2");
-					}
-				}
-			});
-			if (
-				!hamPain1.classList.contains("pain2") &&
-				!hamPain2.classList.contains("pain2")
-			) {
-				hamPain1.classList.add("pain2");
-				hamPain2.classList.add("pain2");
-			} else if (
-				hamPain1.classList.contains("pain2") &&
-				hamPain2.classList.contains("pain2") &&
-				window.scrollY <= 10
-			) {
-				hamPain1.classList.remove("pain2");
-				hamPain2.classList.remove("pain2");
-			}
-		}
 
 		//si l'image est blanche...
 	};
