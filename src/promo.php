93d2d83c268bc85de0daae20d72ea931632b926c
mathieu <mathecool22@gmail.com>
2023-02-06 16:50:01 -0500

    ajout des vrai images pour la version mobile

--- src/promo.php
+++ src/promo.php
@@ -12,14 +12,14 @@ include('header.php');
 
     <div class="swiper-wrapper">
         <div class="swiper-slide"><img src="./images/promo-m-1.png" alt="promo1"></div>
-        <div class="swiper-slide"><img src="./images/promo-1.png" alt="promo2"></div>
-        <div class="swiper-slide"><img src="./images/promo-1.png" alt="promo3"></div>
+        <div class="swiper-slide"><img src="./images/promo-m-2.png" alt="promo2"></div>
+        <div class="swiper-slide"><img src="./images/promo-m-3.png" alt="promo3"></div>
     </div>
 </div>
 </div>
 <section class="veh-carousel">
     <div class="cont-nav-carou cont-nav-carou2">
-        <img src="./images/fleche-carou.svg" alt="prev" class="f-s" id="prev" />
+        <img src="./images/fleche-carou.svg" alt="prev" class="" id="prev" />
         <img src="./images/fleche-carou.svg" alt="next" class="" id="next" />
     </div>
 
